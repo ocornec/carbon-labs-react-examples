b2982ed653c65f8a9679019ca7ec4434a5f9ab9e
owen cornec <o.cornec@ibm.com>
2024-08-26 06:22:20 -0400

    Update AllComponents.js

--- fully-composed-chat/src/AllComponents.js
+++ fully-composed-chat/src/AllComponents.js
@@ -26,7 +26,20 @@ export const Chat = createComponent({
   tagName: 'clabs-chat',
   elementClass: CLABSChat,
   react: React,
-  events: {},
+  events: {
+    onSubmit: 'on-submit',
+    onUserMessageUpdateRequest: 'on-user-message-update-request',
+    onUserRegenerationRequest: 'on-user-regeneration-request',
+    onUserFeedbackRequest: 'on-user-feedback-request',
+    onMessageElementSelected: 'on-message-element-selected',
+    onChatFullscreenChange: 'on-chat-fullscreen-change',
+    onChatDockingChange: 'on-chat-docking-change',
+    onChatClosed: 'on-chat-closed',
+    onHeaderMenuItemSelected: 'on-header-menu-item-selected',
+    onUserStreamInterrupt: 'on-user-stream-interrupt',
+    onMessageStreamingDone: 'on-message-streaming-done',
+    onMessageElementTagSelected: 'on-message-element-tag-selected',
+  },
 });
 
 export const Header = createComponent({
